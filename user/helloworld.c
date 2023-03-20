057411d178063b6cf60e0af085c2cd881b79376d
t-irozenberg <t-irozenberg@microsoft.com>
2023-03-20 11:31:45 +0000

    added hello world call

---
+++ user/helloworld.c
@@ -0,0 +1,11 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main()
+{
+    char* helloWorld = "Hello World xv6\n";
+    write(1, helloWorld, strlen(helloWorld));
+    exit(0);
+}
