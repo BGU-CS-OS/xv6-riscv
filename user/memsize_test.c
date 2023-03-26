1f2b2f43b2f880241dafa2e2fdc357dc28be0712
t-irozenberg <t-irozenberg@microsoft.com>
2023-03-26 10:54:22 +0000

    added memsize sys_call and memsize_test for testing

---
+++ user/memsize_test.c
@@ -0,0 +1,17 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main()
+{
+    int currSize = memsize();
+    printf("Current size: %d\n", currSize);
+    void *allocated = malloc(20 << 10);
+    int newSize = memsize();
+    printf("New size: %d\n", newSize);
+    printf("Difference: %d\n", newSize - currSize);
+    free(allocated);
+    int finalSize = memsize();
+    printf("Final size: %d\n", finalSize);
+    exit(0);
+}
